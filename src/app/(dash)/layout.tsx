3a5784ec155635dcdc57555485624b07d99304c7
Zakir Hussain <itsjakirhussain@gmail.com>
2024-03-10 16:52:06 +0530

    fix

--- src/app/(dash)/layout.tsx
+++ src/app/(dash)/layout.tsx
@@ -9,7 +9,9 @@ export default function POS({ children }: { children: React.ReactNode }) {
     <main className="flex relative min-h-fill w-full h-auto">
       {/* <TopMenu className="col-span-full row-span-1" /> */}
       <SideMenu />
-      <div className="md:p-6 p-4 h-auto w-full bg-paper">{children}</div>
+      <div className="md:px-12 lg:px-24 py-4 px-4 md:py-8 h-auto w-full bg-paper">
+        {children}
+      </div>
     </main>
   );
 }
--- src/components/organisms/tags-box.tsx
+++ src/components/organisms/tags-box.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useMemo, useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import instance from "@/lib/instance";
 import { cn } from "@/lib/utils";
@@ -13,49 +13,72 @@ function TagsBox({ className }: { className?: string }) {
     queryKey: ["tags"],
     queryFn: () => instance.get("/store/tags") as never as any,
   });
+  const itemsPerPage = 12;
+  const [currentPage, setCurrentPage] = useState(1);
+  const hasLoadMore = useMemo(() => {
+    if (data && data.length) {
+      return itemsPerPage * currentPage < data.length;
+    }
+    return false;
+  }, [currentPage, data]);
+
+  const items = useMemo(() => {
+    if (data && data.length && currentPage) {
+      return data.slice(0, itemsPerPage * currentPage);
+    } else {
+      return [];
+    }
+  }, [currentPage, data]);
 
   const baseClass =
-    "w-full grid md:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 grid-cols-1 gap-2 md:gap-4 lg:gap-6";
+    "w-full grid md:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 grid-cols-1 gap-2 md:gap-4 lg:gap-6";
 
   if (isLoading || !data) {
     return (
       <div className={cn(baseClass, className || "")}>
-        {new Array(10).fill(1).map((e, i) => (
+        {new Array(12).fill(1).map((e, i) => (
           <Skeleton key={i} className="h-16 w-full" />
         ))}
+        <div className="col-span-full flex justify-center align-middle items-start py-6">
+          <Skeleton className="w-full md:w-1/4 h-10" />
+        </div>
       </div>
     );
   }
 
   return (
     <div className={cn(baseClass, className || "")}>
-      {data &&
-        data.map(
-          (
-            e: {
-              products: [any];
-              name: string;
-              deck?: string;
-            },
-            i: React.Key | null | undefined
-          ) => (
-            <div
-              className=" bg-background text-foreground relative p-4 rounded-md pr-12"
-              key={i}
+      {items.map(
+        (
+          e: {
+            products: [any];
+            name: string;
+            deck?: string;
+          },
+          i: React.Key | null | undefined
+        ) => (
+          <div
+            className=" bg-background text-foreground relative p-4 rounded-md pr-12"
+            key={i}
+          >
+            <Badge
+              variant={"outline"}
+              className="select-none absolute top-2 right-2"
             >
-              <Badge
-                variant={"outline"}
-                className="select-none absolute top-2 right-2"
-              >
-                {e.products.length || 0}
-              </Badge>
-              <p className="font-semibold">{e.name}</p>
-              <div className="text-foreground/70 text-xs">{e.deck || ""}</div>
-            </div>
-          )
-        )}
+              {e.products.length || 0}
+            </Badge>
+            <p className="font-semibold">{e.name}</p>
+            <div className="text-foreground/70 text-xs">{e.deck || ""}</div>
+          </div>
+        )
+      )}
       <div className="col-span-full flex justify-center align-middle items-start py-6">
-        <Button variant={"outline"} className="w-full md:w-1/4">
+        <Button
+          variant={"outline"}
+          className="w-full md:w-1/4 select-none"
+          disabled={!hasLoadMore}
+          onClick={() => setCurrentPage(currentPage + 1)}
+        >
           Load more...
         </Button>
       </div>
