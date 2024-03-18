b453626b9d84e632a9720e6feeec13c358764a99
Zakir Hussain <itsjakirhussain@gmail.com>
2024-03-18 22:53:46 +0530

    Top menu hider

--- src/app/(dash)/layout.tsx
+++ src/app/(dash)/layout.tsx
@@ -1,17 +1,22 @@
 "use client";
 
+import { useRef } from "react";
 import SideMenu from "@/components/organisms/side-menu";
 import TopMenu from "@/components/organisms/top-menu";
-import { useActionStore } from "@/stores/actionSlice";
-import clsx from "clsx";
+// import { useActionStore } from "@/stores/actionSlice";
 
 export default function POS({ children }: { children: React.ReactNode }) {
-  const minimize = useActionStore((state) => state.isSideBarMinimized);
+  // const minimize = useActionStore((state) => state.isSideBarMinimized);
+  const scrollRef = useRef(null);
+
   return (
     <div className="contents">
       <SideMenu />
-      <main className={"main"}>
-        <TopMenu className="md:hidden block z-30 fixed bg-background w-full" />
+      <main className={"main"} id="scrolls" ref={scrollRef}>
+        <TopMenu
+          className="md:hidden block z-30 fixed bg-background w-full"
+          containerRef={scrollRef}
+        />
         <div className="p-6 h-auto md:h-full w-full bg-paper md:mt-0 mt-[52px]">
           {children}
         </div>
--- src/components/organisms/top-menu.tsx
+++ src/components/organisms/top-menu.tsx
@@ -1,22 +1,48 @@
 "use client";
 
 import clsx from "clsx";
-import { Container } from "@/components/atoms/container";
-import Icon, { IconKey } from "@/components/atoms/icon";
+import Icon from "@/components/atoms/icon";
 import { Button } from "@/components/atoms/button";
 import { useActionStore } from "@/stores/actionSlice";
 import BrandSideBySide from "../atoms/brand/side-by-side";
-import UserMenu from "@/components/organisms/user-menu";
-import { ThemeModeToggle } from "@/components/organisms/theme-mode-toggle";
-import FullScreenButton from "../molecules/full-screen-button";
-import SearchBar from "./search-bar";
+import { MutableRefObject } from "react";
+import { useScroll } from "react-use";
+import { motion } from "framer-motion";
 
-function TopMenu({ className }: { className?: string }) {
+function TopMenu({
+  className,
+  containerRef,
+}: {
+  className?: string;
+  containerRef: MutableRefObject<null>;
+}) {
+  const { y } = useScroll(containerRef);
   const minimize = useActionStore((state) => state.isSideBarMinimized);
   const setMinimize = useActionStore((state) => state.setSideBarMinimize);
 
+  const animator = {
+    hide: {
+      y: -54,
+      transition: {
+        duration: 0.3,
+      },
+    },
+    show: {
+      y: 0,
+      transition: {
+        duration: 0.3,
+      },
+    },
+  };
+
   return (
-    <nav className={clsx("border-b border-paper w-full h-auto", className)}>
+    <motion.nav
+      className={clsx("border-b border-paper w-full h-auto", className)}
+      initial="show"
+      variants={animator}
+      transition={{ type: "spring", stiffness: 100 }}
+      animate={y >= 170 ? "hide" : "show"}
+    >
       <div className="flex justify-center align-middle items-center min-h-[54px] relative">
         <Button
           variant={"transparent"}
@@ -38,7 +64,7 @@ function TopMenu({ className }: { className?: string }) {
           <h1 className="hidden">QuickDine</h1>
         </div>
       </div>
-    </nav>
+    </motion.nav>
   );
 }
 
