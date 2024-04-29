02e7a58b1ea21d49651fa77e954861dc8fbb800f
Zakir Hussain <itsjakirhussain@gmail.com>
2024-04-29 23:51:56 +0530

    fix

--- src/app/(dash)/store/kitchen/page.tsx
+++ src/app/(dash)/store/kitchen/page.tsx
@@ -9,27 +9,33 @@ import {
 export default function Kitchen() {
   return (
     <div className="flex md:flex-row flex-col w-full h-full">
-      <ResizablePanelGroup direction="horizontal" className="w-full h-full">
-        <ResizablePanel defaultSize={50}>
-          <div className="flex h-[200px] items-center justify-center p-6">
-            <span className="font-semibold">One</span>
-          </div>
-        </ResizablePanel>
-        <ResizableHandle />
-        <ResizablePanel defaultSize={50}>
-          <ResizablePanelGroup direction="vertical">
-            <ResizablePanel defaultSize={25}>
-              <div className="flex h-full items-center justify-center p-6">
+      <ResizablePanelGroup direction="vertical">
+        <ResizablePanel defaultSize={75}>
+          <ResizablePanelGroup direction="horizontal" className="w-full h-full">
+            <ResizablePanel defaultSize={35}>
+              <div className="flex h-full items-center justify-center p-6 bg-slate-200">
+                <span className="font-semibold">One</span>
+              </div>
+            </ResizablePanel>
+            <ResizableHandle />
+            <ResizablePanel defaultSize={35}>
+              <div className="flex h-full items-center justify-center p-6 bg-slate-300">
                 <span className="font-semibold">Two</span>
               </div>
             </ResizablePanel>
             <ResizableHandle />
-            <ResizablePanel defaultSize={75}>
-              <div className="flex h-full items-center justify-center p-6">
+            <ResizablePanel defaultSize={30}>
+              <div className="flex h-full items-center justify-center p-6 bg-slate-200">
                 <span className="font-semibold">Three</span>
               </div>
             </ResizablePanel>
           </ResizablePanelGroup>
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel defaultSize={25}>
+          <div className="flex h-full items-center justify-center p-6 bg-slate-100">
+            <span className="font-semibold">Three</span>
+          </div>
         </ResizablePanel>
       </ResizablePanelGroup>
     </div>
